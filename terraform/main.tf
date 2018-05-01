44d4353a19afaa13a0c5d857d18b4ca0a7759e99
scolarofterra <travisdeschenes@gmail.com>
2018-05-01 10:26:35 -0400

    update terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -44,7 +44,7 @@ resource "google_compute_instance_template" "staging-run" {
     gce-container-declaration = <<EOF
 spec:
   containers:
-  - image: 'gcr.io/comp698-tdd1007/github-scolarofterra-comp698-final:92befc2f9e22bb2ae3e7500364a5f6ddfcd78d38'
+  - image: 'gcr.io/comp698-tdd1007/github-scolarofterra-comp698-final:3574888315224521901476dda21214ee597579d5'
     name: service-container
     stdin: false
     tty: false
@@ -65,5 +65,58 @@ resource "google_compute_instance_group_manager" "staging" {
 
 }
 
+resource "google_compute_instance_template" "prod-run" {
+  name_prefix  = "prodrun-"
+  machine_type = "f1-micro"
+  region       = "us-central1"
+  tags = ["http-server"]
+  service_account {
+    scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/cloud-platform",
+      "https://www.googleapis.com/auth/devstorage.read_write",
+    ]
+  }
 
+
+  // boot disk
+  disk {
+    source_image = "cos-cloud/cos-stable"
+  }
+ 
+  network_interface {
+     network = "default"
+
+     access_config {
+          // Ephemeral IP
+    }
+    }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+  metadata {
+    gce-container-declaration = <<EOF
+spec:
+  containers:
+  - image: 'gcr.io/comp698-tdd1007/github-scolarofterra-comp698-final:92befc2f9e22bb2ae3e7500364a5f6ddfcd78d38'
+    name: service-container
+    stdin: false
+    tty: false
+  restartPolicy: Always
+EOF
+  }
+
+}
+
+resource "google_compute_instance_group_manager" "prod" {
+  name               = "prod"
+  instance_template  = "${google_compute_instance_template.prod-run.self_link}"
+  base_instance_name = "prod"
+  zone               = "us-central1-a"
+
+
+  target_size        = "1"
+
+}
 
